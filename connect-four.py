52815be7b4a410d7b56b52214d323209ad9d259e
Goober118 <140124210+Goober118@users.noreply.github.com>
2024-04-04 23:39:22 +0000

    Balls

--- connect-four.py
+++ connect-four.py
@@ -1,11 +1,8 @@
 
-board = []
-
 def initialiseBoard():
     for i in range(7):
         row = ["-","-","-","-","-","-","-"]
         board.append(row)
-
 def displayBoard():
     for i in board:
         print('', end="|")
@@ -13,14 +10,25 @@ def displayBoard():
             print(_, end="|")
         print()
 
+board = []
+playerCount = 1
 initialiseBoard()
 displayBoard()
-playerCount = 0
+
 while True:
-    token = X
+    token = 'X'
     if playerCount % 2 == 0:
-        token = O
-    playerCount += 1
+        token = 'O'
+    choice = int(input("Choose collumn (1-7): "))
+    choice -= 1
+    for i in range(6, -1, -1):
+        if board[i][choice] == '-':
+            board[i][choice] = token
+            playerCount += 1
+            break
+    displayBoard()
+
+
 
 
 
