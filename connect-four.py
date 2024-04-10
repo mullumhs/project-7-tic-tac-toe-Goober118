a47edc8055e606b96be1f9f46587b041d5929a48
Goober118 <140124210+Goober118@users.noreply.github.com>
2024-04-10 00:38:45 +0000

    Balls

--- connect-four.py
+++ connect-four.py
@@ -1,36 +1,52 @@
 
+
+
 def initialiseBoard():
-    for i in range(7):
+    board = []
+    for i in range(6):
         row = ["-","-","-","-","-","-","-"]
         board.append(row)
-def displayBoard():
+    return board
+
+def displayBoard(board):
+    print("|1|2|3|4|5|6|7|")
     for i in board:
         print('', end="|")
         for _ in i:
             print(_, end="|")
         print()
 
-board = []
-playerCount = 1
-initialiseBoard()
-displayBoard()
-
-while True:
-    token = 'X'
-    if playerCount % 2 == 0:
-        token = 'O'
-    choice = int(input("Choose collumn (1-7): "))
-    choice -= 1
-    for i in range(6, -1, -1):
-        if board[i][choice] == '-':
-            board[i][choice] = token
-            playerCount += 1
-            break
-        
+def horizontalWinCheck(board):
     for row in range(6):
         for col in range(4):
             if board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] and not board[row][col + 3] == '-':
                 print("win")
                 break
 
-    displayBoard()
+def verticalWinCheck(board):
+     for col in range(7):
+         for row in range (4):
+             if board[col][row] == board[col][row + 1] == board[col][row + 2] == board
+def tokenCheck(board, playerCount):
+        token = 'X'
+        if playerCount % 2 == 0:
+            token = 'O'
+        choice = int(input("Choose collumn (1-7): "))
+        choice -= 1
+        for i in range(5, -1, -1):
+            if board[i][choice] == '-':
+                board[i][choice] = token
+                playerCount += 1
+                return playerCount
+
+def main():
+
+    playerCount = 1         
+    board = initialiseBoard()
+    displayBoard(board)
+    while True:
+        playerCount = tokenCheck(board, playerCount)
+        horizontalWinCheck(board)
+        displayBoard(board)
+
+main()
