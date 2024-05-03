e86b5565aac2d37cd475950f8fb336a6fb665724
Goober118 <140124210+Goober118@users.noreply.github.com>
2024-05-03 00:40:25 +0000

    balls

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,22 +1,23 @@
 def main():
         
-        playerCount = 1
-        board = initialiseBoard()
+    playerCount = 1
+    board = initialiseBoard()
+    displayBoard(board)
+    while True:
+        playerCount = tokenCheck(board, playerCount)
         displayBoard(board)
-        while True:
-            playerCount = tokenCheck(board, playerCount)
-            displayBoard(board)
+        winCheck(board)
     # Your code goes here
 
 def initialiseBoard():
     board = []
     for i in range(3):
-        row = ["_","_","_"]
+        row = [" - "," - "," - "]
         board.append(row)
     return board
 
 def displayBoard(board):
-    print("|1|2|3|")
+    print("| 1 | 2 | 3 |")
     for i in board:
         print('', end="|")
         for _ in i:
@@ -25,18 +26,48 @@ def displayBoard(board):
 
 def tokenCheck(board, playerCount):
     
-    token = 'X'
+    token = ' X '
     if playerCount % 2 == 0:
-        token = 'O'
+        token = ' O '
     choice_row = int(input("Input a row from 1-3: "))
     choice_row -= 1
     choice_col = int(input("Input a collumn from 1-3: "))
     choice_col -= 1
-    for i in range(2,-1,-1):
-        if board[choice_row][choice_col] == '-':
-            board[choice_row][choice_col] = token
-            playerCount += 1
-            return playerCount
+    if board[choice_row][choice_col] == ' - ':
+        board[choice_row][choice_col] = token
+        playerCount += 1
+    else:
+        print("Error, token already placed there")
+    return playerCount
+
+def winCheck(board):
+
+    #Check rows
+
+    # Check first row
+    if board[0][0] == board[0][1] == board[0][2] != ' - ':
+        return True
+    # Check second row
+    if board[1][0] == board[1][1] == board[1][2] != ' - ':
+        return True
+    # Check third row
+    if board[2][0] == board[2][1] == board[2][2] != ' - ':
+        return True
+    
+    #Check collumns   
+
+    # Check first collumn
+    if board[0][0] == board[1][0] == board[2][0] != ' - ':
+    # Check second collumn
+    if board[0][1] == board[1][1] == board[2][1] != ' - ':
+    # Check third collumn
+    if board[0][2] == board[1][2] == board[1][3] != ' - ':
+
+    # Check diagonals
+
+    # Check bottom diagonal
+    if board[0][0] == board[0][1] == board[0][2] != ' - ':
+    if board[0][0] == board[0][1] == board[0][2] != ' - ':
 
 
 main()
